8150e1e135163c3d208c5993103a493cd9fa6358
FuStar734 <81980959+FuStar734@users.noreply.github.com>
2021-05-15 11:38:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function stuff() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stuff()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var navbar = document.getElementById("mynavbar");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = navbar.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stuff() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    navbar.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
